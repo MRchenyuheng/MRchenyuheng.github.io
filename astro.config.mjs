0ad141383014085dbb17c9c8a4d5d86eb1dd5cbf
MRchenyuheng <1351965541@qq.com>
2025-03-21 15:47:20 +0800

    https://lemontrees.top/

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https:MRchenyuheng.github.io",
-  base: "/",
+  site: "https://lemontrees.top/",
+  // base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
