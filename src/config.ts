817f365d5fdc7d9428d15a2019c9797e0a3e04b8
MRchenyuheng <1351965541@qq.com>
2026-02-25 15:46:00 +0800

    archive/tag

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const navBarConfig: NavBarConfig = {
     // 新增自定义菜单项：408统考
     {
       name: '408统考',
-      url: '/categories/408统考/', // 对应你的分类页面路径
+      url: '/archive/tag/408统考/', // 对应你的分类页面路径
       external: false, // 内部链接，不显示外链图标
     },
     // 新增自定义菜单项：music
